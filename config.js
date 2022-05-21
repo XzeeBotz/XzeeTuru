1439ecca56b6ef072ac85b9f1a2c610eaae04ec0
XzeeBotz <100888117+XzeeBotz@users.noreply.github.com>
2022-05-21 10:54:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6281515860089', '6281334177340', '6283128734012'] // Put your number here
+global.owner = ['6281381685954', '6289514712995', '6285755734716'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -27,8 +27,8 @@ global.APIKeys = { // APIKey Here
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'I hope you\'re fine'
-  var sticker_author = 'Nurutomo'
+  var sticker_name = 'Yang Nyolong Pantat Nya Rapet'
+  var sticker_author = 'DipaLand'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
